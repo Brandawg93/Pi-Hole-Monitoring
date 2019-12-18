5ea494b3503c7ed2d1590f9554b06897eec12894
brandawg93 <brandonmcfarlin1016@gmail.com>
2019-12-18 17:05:33 -0500

    fix dashboards

--- sql_influx/main.py
+++ sql_influx/main.py
@@ -5,58 +5,76 @@ import time
 import json
 import requests
 
-row = None
-last_id = None
-query = "SELECT * FROM queries WHERE id = (SELECT MAX(id) FROM queries)"
 client = InfluxDBClient(host='influxdb', port=8086, database='pihole-FTL')
 
-while True:
-	try:
-		client.create_database('pihole-FTL')
-		break
-	except requests.exceptions.ConnectionError:
-		print('Could not connect. Retrying...')
-		time.sleep(5)
-	except KeyboardInterrupt:
-		sys.exit(0)
 
-while True:
-	try:
-		con = connect('/etc/pihole/pihole-FTL.db')
-		with con:
-			if last_id:
-				query = "SELECT * FROM queries WHERE id > {}".format(last_id)
-			cur = con.cursor()
-			cur.execute(query)
+def wait_for_connection():
+	'''wait for a connection from the influxdb'''
+	while True:
+		try:
+			client.create_database('pihole-FTL')
+			break
+		except requests.exceptions.ConnectionError:
+			print('Could not connect. Retrying...')
+			time.sleep(5)
+		except KeyboardInterrupt:
+			sys.exit(0)
 
-			if row:
-				last_id = row[0]
-			rows = cur.fetchall()
 
-			if len(rows) > 0:
-				row = rows[-1]
-				id = row[0]
+def get_last_id():
+	'''fill existing data from pihole-FTL.db'''
+	rs = list(client.query('SELECT last(id) FROM "pihole-FTL";').get_points())
+	last_id = 0
+	if len(rs) > 0:
+		last_id = rs[0]['last']
+	return last_id
 
-			if row and id != last_id:
-				for item in rows:
-					json_body = [
-						{
-							"measurement": "pihole-FTL",
-							"tags": {
-								"type": item[2],
-								"status": item[3],
-								"domain": item[4],
-								"client": item[5],
-								"forward": item[6]
-							},
-							"time": time.strftime('%Y-%m-%dT%H:%M:%S', time.localtime(item[1])),
-							"fields": {
-								"id": item[0]
+
+def add_new_results(last_id):
+	'''write new results from pihole-FTL.db'''
+	row = None
+	print('filling data...')
+	while True:
+		try:
+			con = connect('/etc/pihole/pihole-FTL.db')
+			with con:
+				query = "SELECT * FROM queries WHERE id > {} ORDER BY id DESC LIMIT 10000".format(last_id)
+				cur = con.cursor()
+				cur.execute(query)
+
+				if row:
+					last_id = row[0]
+				rows = cur.fetchall()
+
+				if len(rows) > 0:
+					row = rows[0]
+					id = row[0]
+
+				if row and id != last_id:
+					for item in rows:
+						json_body = [
+							{
+								"measurement": "pihole-FTL",
+								"tags": {
+									"type": item[2],
+									"status": item[3],
+									"domain": item[4],
+									"client": item[5],
+									"forward": item[6]
+								},
+								"time": time.strftime('%Y-%m-%dT%H:%M:%S', time.localtime(item[1])),
+								"fields": {
+									"id": item[0]
+								}
 							}
-						}
-					]
-					client.write_points(json_body)
-			else:
-				time.sleep(5)
-	except KeyboardInterrupt:
-		break
+						]
+						client.write_points(json_body)
+				else:
+					time.sleep(5)
+		except KeyboardInterrupt:
+			break
+
+if __name__ == "__main__":
+	wait_for_connection()
+	last_id = get_last_id()
+	add_new_results(last_id)
